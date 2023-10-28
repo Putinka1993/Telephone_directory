fc48a71542b3ca3bd7e0b95f2682a973f37d2b97
VladislavLipkinGit <lipkin1993vladislav@gmail.com>
2023-10-28 23:57:29 +0300

    add function Print_the_guide and Find_phone

--- main.py
+++ main.py
@@ -10,3 +10,80 @@ def selective_menu():
     )
     choice = int(input("введите номер запроса:   "))
     return choice
+
+#          ------ работа в справочнике ------
+
+def Queries_in_the_directory():
+
+    choice = selective_menu()
+    #          ------ функции справочника ------
+
+    # распечатать справочник
+    def Print_the_guide():
+        with open('numbers.txt', 'r', encoding='utf-8') as data_numbers:
+            print(data_numbers.read())
+            print()
+
+    # найти телефон
+    def Find_phone():
+        with open('numbers.txt', 'r') as file:
+            data = file.readlines()
+
+        find = input("Введите Фамилию или Имя:   ").lower()
+
+        print(f" Результаты поиска по запросу: {find}")
+        for row in data:
+            line = list(row.split(","))
+
+            if find == "":
+                print(f"По запросу {find} ничего не найдено.")
+                break
+            elif find in line[0].lower() or find in line[1].lower():  # or line[1].lower():
+                print(f"{line[0]} {line[1]} - {int(line[2])}")
+                break
+        else:
+            print(f"По запросу {find} ничего не найдено.")
+        print()
+
+        print(
+            '1. Найти телефон',
+            'Enter. Выход в главное меню',
+            sep='\n'
+        )
+        req = int(input("Введите номер запроса:   "))
+
+        match req:
+            case 1:
+                Find_phone()
+
+
+
+    # изменить номер телефона
+    def Change_phone_number():
+        return
+
+    # удалить запись
+    def Delete_entry():
+        return
+
+    # найти абонента по номеру телефона
+    def Find_a_subscriber_by_phone_number():
+        return
+
+    # добавить абонента в справочник
+    def Add_a_subscriber_to_the_directory():
+        return
+
+    while (choice != 7):
+
+        match choice:
+            case 1:
+                Print_the_guide()
+            case 2:
+                Find_phone()
+
+        choice = selective_menu()
+
+
+
+Queries_in_the_directory()
