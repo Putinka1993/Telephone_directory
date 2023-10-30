7ce76cbd9d32f0132368b7ac1827cfcd89b48eeb
VladislavLipkinGit <lipkin1993vladislav@gmail.com>
2023-10-30 22:07:27 +0300

    add function Delete_entry

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+import re
+
+
 def selective_menu():
     print(
         '1. Распечатать справочник',
@@ -107,7 +110,34 @@ def Queries_in_the_directory():
 
     # удалить запись
     def Delete_entry():
-        return
+
+        with open('test_phone.txt') as f:
+            lines = f.readlines()
+
+        delete_contact = input("Выберете контакт который хотите удалить:   ").lower()
+        pattern = re.compile(re.escape(delete_contact))
+
+        with open('test_phone.txt', 'w') as f:
+            count = 0
+            for line in lines:
+                result = pattern.search(line.lower())
+                if result is None:
+                    f.write(line)
+                    count += 1
+
+            if count == len(lines):
+                print(f"Результатов по запросу {delete_contact} нету в записной книжке")
+
+            print(
+                '1. Найти телефон',
+                'Enter. Выход в главное меню',
+                sep='\n'
+            )
+            req = input("Введите номер запроса:   ")
+
+            match req:
+                case '1':
+                    Delete_entry()
 
     # найти абонента по номеру телефона
     def Find_a_subscriber_by_phone_number():
--- test/test.py
+++ test/test.py
@@ -1,55 +1,36 @@
-
-def Change_phone_number():
-
-    with open('test_phone.txt', 'r') as file:
-        data = file.readlines()
-
-    print("Смена номера:")
-    find = input("Введите Фамилию или Имя абонента:   ").lower()
-
-    print(f" Результаты поиска по запросу: {find}")
-    for row in data:
-        line = list(row.split(","))
-
-        if find == "":
-            print(f"По запросу {find} ничего не найдено.")
-            break
-        elif find in line[0].lower() or find in line[1].lower():
-            print(f"{line[0]} {line[1]} - {int(line[2])}")
-
-            with open('test_phone.txt', 'r') as f:
-                old_data = f.read()
-
-            new_number = input(f"Введите новый номер абонента {line[0]} {line[1]}")
-
-            if new_number == "":
-                print("Неккоректно веден номер")
-                Change_phone_number()
-            else:
-                new_data = old_data.replace(line[2], new_number)
-
-                with open('test_phone.txt', 'w') as f:
-                    f.write(new_data)
-                print(f"Номер успешно изменен на {new_number}")
-                break
-    else:
-        print(f"По запросу {find} ничего не найдено.")
-    print()
-
-    print(
-        '1. Найти телефон',
-        'Enter. Выход в главное меню',
-        sep='\n'
-    )
-    req = input("Введите номер запроса:   ")
-
-    match req:
-        case '1':
-            Change_phone_number()
+import re
 
 
+def Delete_entry():
 
 
+    with open('test_phone.txt') as f:
+        lines = f.readlines()
+
+    delete_contact = input("Выберете контакт который хотите удалить:   ").lower()
+    pattern = re.compile(re.escape(delete_contact))
+
+    with open('test_phone.txt', 'w') as f:
+        count = 0
+        for line in lines:
+            result = pattern.search(line.lower())
+            if result is None:
+                f.write(line)
+                count += 1
+
+        if count == len(lines):
+            print(f"Результатов по запросу {delete_contact} нету в записной книжке")
+
+        print(
+            '1. Найти телефон',
+            'Enter. Выход в главное меню',
+            sep='\n'
+        )
+        req = input("Введите номер запроса:   ")
+
+        match req:
+            case '1':
+                Delete_entry()
 
 
-Change_phone_number()
+Delete_entry()
